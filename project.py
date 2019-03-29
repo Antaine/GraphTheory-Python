1ea521e74499766900080d16e15304563c1c80e6
AntoConn <g00347577@gmit.ie>
2019-03-29 00:37:59 +0000

    Split Input into Character List

--- project.py
+++ project.py
@@ -2,3 +2,9 @@
 import sys
 input1 = input("Enter expression: ")
 print(input1)
+inputList = list(input1)
+#inputList = list(input)
+print(inputList)
+
+# for temp in inputList:   
+#     print(temp)
